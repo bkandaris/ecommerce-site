48ae72e1038c6a463cadf51df88d63e410231bb8
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-03 18:15:28 -0400

    cart complete

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { Add, Remove } from '@material-ui/icons';
 
 const Cart = () => {
   return (
@@ -21,7 +22,7 @@ const Cart = () => {
           <Info>
             <Product>
               <ProductDetail>
-                <Image src='https://images.unsplash.com/photo-1558888401-3cc1de77652d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80' />
+                <Image src='https://images.unsplash.com/photo-1586899028174-e7098604235b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1471&q=80' />
                 <Details>
                   <ProductName>
                     <b>Product:</b> Samsung TV
@@ -35,10 +36,62 @@ const Cart = () => {
                   </ProductSize>
                 </Details>
               </ProductDetail>
-              <PriceDetail>price</PriceDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <Remove />
+                  <ProductAmount>2</ProductAmount>
+                </ProductAmountContainer>
+                <ProductPrice>$20</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src='https://images.unsplash.com/photo-1586899028174-e7098604235b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1471&q=80' />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> Samsung TV
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 5651899818
+                  </ProductId>
+                  <ProductBrand />
+                  <ProductSize>
+                    <b>Size:</b> 37.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <Remove />
+                  <ProductAmount>2</ProductAmount>
+                </ProductAmountContainer>
+                <ProductPrice>$20</ProductPrice>
+              </PriceDetail>
             </Product>
           </Info>
-          <Summary>Summary</Summary>
+          <Summary>
+            <SummaryTitle>Order Summary</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$80</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$6.00</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>-$6.00</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type='total'>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$80</SummaryItemPrice>
+            </SummaryItem>
+            <Button>Checkout</Button>
+          </Summary>
         </Bottom>
       </Wrapper>
 
@@ -89,21 +142,89 @@ const Bottom = styled.div`
   justify-content: space-between;
 `;
 
+const Hr = styled.hr`
+  background-color: darkgrey;
+  border: none;
+  height: 1px;
+`;
+
 const Info = styled.div`
   flex: 3;
 `;
 
 const Summary = styled.div`
   flex: 1;
-  background-color: lightblue;
+  border: 0.5px solid lightgrey;
+  border-radius: 10px;
+  padding: 20px;
+  height: 30vh;
 `;
 
-const Product = styled.div``;
-const ProductDetail = styled.div``;
-const Image = styled.div``;
-const Details = styled.div``;
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
 const ProductName = styled.span``;
 const ProductId = styled.span``;
 const ProductBrand = styled.div``;
 const ProductSize = styled.span``;
-const PriceDetail = styled.div``;
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+const SummaryTitle = styled.h2`
+  font-weight: bold;
+`;
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === 'total' && '500'};
+  font-size: ${(props) => props.type === 'total' && '25px'};
+`;
+
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: bold;
+`;
